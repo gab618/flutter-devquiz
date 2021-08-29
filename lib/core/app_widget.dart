78810798a2287c1c5f1f4b34ac132dff08bd290f
Gabriel <knuckles4x@gmail.com>
2021-08-29 11:20:35 -0300

    imports

---
+++ lib/core/app_widget.dart
@@ -0,0 +1,14 @@
+// import 'package:devquiz2/home/home_page.dart';
+// import 'package:devquiz2/splash/splash_page.dart';
+import 'package:devquiz2/splash/splash_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DevQuiz",
+      home: SplashPage(),
+    );
+  }
+}
